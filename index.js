fbff614246843adf2f490f564cc3c9f65fa07c42
Shanmuk <boorashanmukkumar.128697@marwadiuniversity.ac.in>
2026-03-16 11:56:35 +0530

    Updated index.js for practical 4

--- index.js
+++ index.js
@@ -2,24 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/api/info", (req, res) => {
-  res.json({
-    name: "npm-lab",
-    version: "1.0.0",
-    description: "Practical 4 API info"
-  });
-});
-
-app.get("/api/user", (req, res) => {
-  res.json({
-    id: 1,
-    name: "Shanmuk Kumar",
-    course: "Open Source Technologies"
-  });
-});
-
 app.get("/", (req, res) => {
-  res.send("Server running at http://localhost:3000");
+  res.send("<h1>Hello Open Source Tech</h1>");
 });
 
 app.listen(PORT, () => {
